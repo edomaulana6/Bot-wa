b60c84a9fc3d891ab7b5b2fd24c0f43a024a2b4f
Leonel Macejkovic <edom70976@gmail.com>
2026-02-26 07:12:58 +0700

    Update and rename setup.sh to package.json

--- setup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Skrip Setup Otomatis Ubuntu
-sudo apt update
-sudo apt install -y python3 python3-pip ffmpeg
-pip3 install -r requirements.txt
-echo "Setup selesai. Jalankan bot dengan: python3 main.py"
