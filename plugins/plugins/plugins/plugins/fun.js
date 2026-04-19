ceccca4ff96745586041fb4a889dc9660d538c22
Leonel Macejkovic <edom70976@gmail.com>
2026-04-19 08:09:56 +0700

    Delete plugins/plugins directory

--- plugins/plugins/plugins/plugins/fun.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    command: ['alay', 'cekmati', 'halu', 'truth', 'dare', 'apakah', 'siapakah', 'kapankah', 'quotes', 'owner'],
-    operate: async (sock, msg, from, args, { pushname }) => {
-        const replies = ["Ya", "Tidak", "Mungkin", "Bisa jadi"];
-        const rand = replies[Math.floor(Math.random() * replies.length)];
-        await sock.sendMessage(from, { text: `Pertanyaan: ${args.join(" ")}\nJawaban: ${rand}` });
-    }
-};
